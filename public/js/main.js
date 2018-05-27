76c2baf59448a8cab2da8694d96730a880bf12cb
Andrew <andrewe456@gmail.com>
2018-05-27 00:04:27 -0700

    Nav Bar Functionality
    Functionality should be pretty much there. Need to adjust some of the styling still

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,28 @@
 function toggleMobileNav() {
-  $("#MobMenu").toggleClass('mob-nav--closed').toggleClass('mob-nav--open')
+  if ($("#MobMenu").hasClass("mob-nav--closed")) {
+    $("#MobMenu").removeClass("mob-nav--closed")
+    $("#MobMenu").addClass("mob-nav--open")
+  }
+  else{
+    $("#MobMenu").removeClass('mob-nav--open')
+    $("#MobMenu").addClass('mob-nav--closed')
+  }
+}
+
+function toggleMenu(childMenu) {
+  parentMenu = document.getElementById(childMenu).dataset.parent
+  console.log(parentMenu)
+  document.getElementById(parentMenu).classList.toggle('mob-nav__menu--closed')
+  document.getElementById(parentMenu).classList.toggle('mob-nav__menu--open')
+  $("#"+childMenu).toggleClass('mob-nav__menu--closed').toggleClass('mob-nav__menu--open')
 }
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('#MobMenuToggle') 
-    && $("#MobMenu").hasClass('mob-nav--open')
-    && !$(event.target).hasClass('mob-nav__menu') ) {
+  if (event.target.matches('#burger') || !$("#MobMenu").hasClass('mob-nav--open') || $.contains(document.getElementById("MobMenu") , event.target) ) {
+    return
+  }
+  else {
     $("#MobMenu").addClass('mob-nav--closed').removeClass('mob-nav--open')
   }
 }
