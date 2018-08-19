96e6020b156b20f5d977a10e91d586652dbf1639
Andrew <andrewe456@gmail.com>
2018-08-18 17:25:40 -0700

    mission widget, mobile nav sizing

--- public/js/main.js
+++ public/js/main.js
@@ -26,38 +26,3 @@ window.onclick = function(event) {
     $("#MobMenu").addClass('mob-nav--closed').removeClass('mob-nav--open')
   }
 }
-
-//mission highlight function
-var buffer = 300
-$(document).ready(function(){
-  $(window).scroll(function() {
-    var top = $(document).scrollTop()
-    if (top > ($("#mission1").position().top - buffer) && top < ($("#mission2").position().top - buffer)){
-      //console.log("here 1")
-      $("#mission1_image").show()
-      $("#mission2_image").hide()
-      $("#mission3_image").hide()
-      $("#mission4_image").hide()
-    }
-    else if (top >  $("#mission2").position().top - buffer && top < ($("#mission3").position().top - buffer)) {
-      //console.log("here 2")
-      $("#mission1_image").hide()
-      $("#mission2_image").show()
-      $("#mission3_image").hide()
-      $("#mission4_image").hide()
-    }
-    else if (top > $("#mission3").position().top - buffer && top < ($("#mission4").position().top - buffer)) {
-      //console.log("here 3")
-      $("#mission1_image").hide()
-      $("#mission2_image").hide()
-      $("#mission3_image").show()
-      $("#mission4_image").hide()
-    } else if (top > $("#mission4").position().top - buffer) {
-      //console.log("here 3")
-      $("#mission1_image").hide()
-      $("#mission2_image").hide()
-      $("#mission3_image").hide()
-      $("#mission4_image").show()
-    }
-  });
-});
