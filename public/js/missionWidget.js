165aad875b9e95d0f008aa2421dfd2c85bd4dc7c
Andrew <andrewe456@gmail.com>
2018-09-13 15:40:47 -0700

    Project page changes, calendar page changes, probs a few others

--- public/js/missionWidget.js
+++ public/js/missionWidget.js
@@ -7,32 +7,36 @@ $(document).ready(function(){
 
   $(window).scroll(function() {
     var top = $(document).scrollTop()
-    if (top > ($("#mission1").position().top - buffer) && top < ($("#mission2").position().top - buffer)){
-      //console.log("here 1")
-      $("#mission1_image").show()
-      $("#mission2_image").hide()
-      $("#mission3_image").hide()
-      $("#mission4_image").hide()
-    }
-    else if (top >  $("#mission2").position().top - buffer && top < ($("#mission3").position().top - buffer)) {
-      //console.log("here 2")
-      $("#mission1_image").hide()
-      $("#mission2_image").show()
-      $("#mission3_image").hide()
-      $("#mission4_image").hide()
-    }
-    else if (top > $("#mission3").position().top - buffer && top < ($("#mission4").position().top - buffer)) {
-      //console.log("here 3")
-      $("#mission1_image").hide()
-      $("#mission2_image").hide()
-      $("#mission3_image").show()
-      $("#mission4_image").hide()
-    } else if (top > $("#mission4").position().top - buffer) {
-      //console.log("here 3")
-      $("#mission1_image").hide()
-      $("#mission2_image").hide()
-      $("#mission3_image").hide()
-      $("#mission4_image").show()
+    try {
+      if (top > ($("#mission1").position().top - buffer) && top < ($("#mission2").position().top - buffer)){
+        //console.log("here 1")
+        $("#mission1_image").show()
+        $("#mission2_image").hide()
+        $("#mission3_image").hide()
+        $("#mission4_image").hide()
+      }
+      else if (top >  $("#mission2").position().top - buffer && top < ($("#mission3").position().top - buffer)) {
+        //console.log("here 2")
+        $("#mission1_image").hide()
+        $("#mission2_image").show()
+        $("#mission3_image").hide()
+        $("#mission4_image").hide()
+      }
+      else if (top > $("#mission3").position().top - buffer && top < ($("#mission4").position().top - buffer)) {
+        //console.log("here 3")
+        $("#mission1_image").hide()
+        $("#mission2_image").hide()
+        $("#mission3_image").show()
+        $("#mission4_image").hide()
+      } else if (top > $("#mission4").position().top - buffer) {
+        //console.log("here 3")
+        $("#mission1_image").hide()
+        $("#mission2_image").hide()
+        $("#mission3_image").hide()
+        $("#mission4_image").show()
+      }
+    } catch(err) {
+      console.log(err)
     }
   });
 });
