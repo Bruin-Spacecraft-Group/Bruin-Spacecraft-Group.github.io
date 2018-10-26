3c1d9b6b276968778b40a8cbc8b59b234083d8f6
Andrew <andrewe456@gmail.com>
2018-10-25 21:25:29 -0700

    fixed mob nav

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,36 @@
-function toggleMobileNav() {
-  if ($("#MobMenu").hasClass("mob-nav--closed")) {
-    $("#MobMenu").removeClass("mob-nav--closed")
-    $("#MobMenu").addClass("mob-nav--open")
+// Toggle dropdown menu
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  console.log(event.target)
+  if (event.target.matches('#mob-nav__container') || event.target.matches('#mob-nav__burger')
+    || event.target.matches('#burger') || event.target.matches('#MobMenuToggle')){
+    if ($('#MobMenu').hasClass('mob-nav--open')){
+      $('#MobMenu').removeClass('mob-nav--open')
+      $('#MobMenu').addClass('mob-nav--closed')
+      console.log("closing")
+    }
+    else{
+      $('#MobMenu').addClass('mob-nav--open')
+      $('#MobMenu').removeClass('mob-nav--closed')
+      console.log("opening")
+    }
   }
-  else{
-    $("#MobMenu").removeClass('mob-nav--open')
-    $("#MobMenu").addClass('mob-nav--closed')
+  else {
+    console.log($(event.target).closest("#MobMenu").length)
+    if ($(event.target).closest("#MobMenu").length > 0){
+      console.log("leaving open")
+    }
+    else if ($('#MobMenu').hasClass('mob-nav--open')){
+      $('#MobMenu').removeClass('mob-nav--open')
+      $('#MobMenu').addClass('mob-nav--closed')
+      console.log("closing")
+    }
+    else{
+      console.log("do nothing")
+    }
   }
 }
+
 
 function toggleMenu(childMenu) {
   parentMenu = document.getElementById(childMenu).dataset.parent
@@ -15,14 +38,4 @@ function toggleMenu(childMenu) {
   document.getElementById(parentMenu).classList.toggle('mob-nav__menu--closed')
   document.getElementById(parentMenu).classList.toggle('mob-nav__menu--open')
   $("#"+childMenu).toggleClass('mob-nav__menu--closed').toggleClass('mob-nav__menu--open')
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (event.target.matches('#burger') || !$("#MobMenu").hasClass('mob-nav--open') || $.contains(document.getElementById("MobMenu") , event.target) ) {
-    return
-  }
-  else {
-    $("#MobMenu").addClass('mob-nav--closed').removeClass('mob-nav--open')
-  }
 }
