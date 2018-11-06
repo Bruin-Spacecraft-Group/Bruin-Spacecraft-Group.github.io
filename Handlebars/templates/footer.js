4cfcf4969836e0410bc7ad85f523a4be83a673fc
Andy Peng <andypg20@gmail.com>
2018-11-05 21:57:29 -0800

    Redid footer social media icons to add more padding and stagger size decrease

--- Handlebars/templates/footer.js
+++ Handlebars/templates/footer.js
@@ -1,11 +1,20 @@
 var context = {
   'socialMedia': [
-    {'element':`<a href="https://www.facebook.com/uclaBruinSpace/" target="_blank"><img src="${links.icons}/facebook.png"></a>`},
-    {'element':`<a href="https://www.instagram.com/uclabruinspace/" target="_blank"><img src="${links.icons}/instagram.png"></a>`},
-    {'element':`<a href="https://twitter.com/uclabruinspace" target="_blank"><img src="${links.icons}/twitter.png"></a>`},
-    {'element':`<a href="https://www.linkedin.com/company/bruin-spacecraft-group-at-ucla/" target="_blank"><img src="${links.icons}/linkedin.png"></a>`}
-  ]
-}
+    {
+      'link': 'https://www.facebook.com/uclaBruinSpace/',
+      'icon': `${links.icons}/facebook.png`,
+    }, {
+      'link': 'https://www.instagram.com/uclabruinspace/',
+      'icon': `${links.icons}/instagram.png`,
+    }, {
+      'link': 'https://twitter.com/uclabruinspace',
+      'icon': `${links.icons}/twitter.png`,
+    }, {
+      'link': 'https://www.linkedin.com/company/bruin-spacecraft-group-at-ucla/',
+      'icon': `${links.icons}/linkedin.png`,
+    },
+  ],
+};
 
 var templateScript = Handlebars.templates.footer(context);
 
--- Handlebars/templates/templatesCompiled.js
+++ Handlebars/templates/templatesCompiled.js
@@ -1,11 +1,13 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['footer'] = template({"1":function(container,depth0,helpers,partials,data) {
-    var stack1, helper;
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "    "
-    + ((stack1 = ((helper = (helper = helpers.element || (depth0 != null ? depth0.element : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"element","hash":{},"data":data}) : helper))) != null ? stack1 : "")
-    + "\n";
+  return "    <a href="
+    + alias4(((helper = (helper = helpers.link || (depth0 != null ? depth0.link : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"link","hash":{},"data":data}) : helper)))
+    + " target=\"_blank\">\n      <img src="
+    + alias4(((helper = (helper = helpers.icon || (depth0 != null ? depth0.icon : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"icon","hash":{},"data":data}) : helper)))
+    + ">\n    </a>\n";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1;
 
