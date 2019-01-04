e0b7f6399ab19d7b9a9e2c7947840ca18b6b555b
Andrew <andrewe456@gmail.com>
2019-01-03 16:17:25 -0800

    few link typos, and did the officer card css

--- templates/our_team.js
+++ templates/our_team.js
@@ -4,31 +4,43 @@ var context = {
       'name':     'Andrew Evans',
       'position': 'President',
       'picture':  `${links.people}/andrew.jpg`,
-      'about':    `<p>Yeah, I basically do everything, so that's fun</p>`
+      'about':    `<p>Third Year, Aerospace Engineering</p>
+      <p>I joined this club my freshman year, working on the Ops team for Project Reach. This is now my second year as president, and I'm still learning something new everyday.</p>
+      <p>I am passionate about science and engineering, and just generally building cool things!</p>`,
+      'link': 'https://linkedin.com/in/andrew-evans/',
+      'icon': `${links.icons}/linkedin.png`
     },
     {
       'name':     'Nadine Tabucol',
       'position': 'Internal Vice President',
       'picture':  `${links.people}/nadine.jpg`,
-      'about':    ''
+      'about':    '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis sollicitudin leo, a luctus nisi. Proin placerat nec velit nec fringilla. Etiam mollis nulla ut nibh venenatis, ac efficitur nisl aliquet. Nunc pharetra rhoncus purus eget ullamcorper. Donec vel massa eu leo pellentesque mattis. Etiam lorem augue, iaculis sit amet metus sed, placerat tempus felis. Ut id elit magna. Cras elementum nunc euismod, ultricies elit vitae, suscipit lectus. Pellentesque ante elit, bibendum porta mauris non, luctus placerat magna.</p>',
+      'link': 'https://www.linkedin.com/company/bruin-spacecraft-group-at-ucla/',
+      'icon': `${links.icons}/linkedin.png`
     },
     {
       'name':     'Eva Abramson',
       'position': 'External Vice President',
       'picture':  `${links.people}/eva.jpg`,
-      'about':    ''
+      'about':    '',
+      'link': 'https://www.linkedin.com/company/bruin-spacecraft-group-at-ucla/',
+      'icon': `${links.icons}/linkedin.png`
     },
     {
       'name':     'Owen Weitzel',
       'position': 'Vice President of Marketing',
       'picture':  `${links.people}/owen.jpg`,
-      'about':    ''
+      'about':    '',
+      'link': 'https://www.linkedin.com/company/bruin-spacecraft-group-at-ucla/',
+      'icon': `${links.icons}/linkedin.png`
     },
     {
       'name':     'Daniel Hu',
       'position': 'Vice President of Finance',
       'picture':  `${links.people}/daniel.jpg`,
-      'about':    '<p>My only friend</p>'
+      'about':    '<p>My only friend</p>',
+      'link': 'https://www.linkedin.com/company/bruin-spacecraft-group-at-ucla/',
+      'icon': `${links.icons}/linkedin.png`
     }
   ],
   'teams': [
