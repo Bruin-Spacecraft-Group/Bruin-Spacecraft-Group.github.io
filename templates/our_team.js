0ec30f9599c13acc3d94479340a3c6c93224ed70
Andrew <andrewe456@gmail.com>
2019-01-07 18:40:20 -0800

    updated bios

--- templates/our_team.js
+++ templates/our_team.js
@@ -11,19 +11,19 @@ var context = {
       'icon': `${links.icons}/linkedin.png`
     },
     {
-      'name':     'Nadine Tabucol',
+      'name':     'Karthik Pullela',
       'position': 'Internal Vice President',
-      'picture':  `${links.people}/nadine.jpg`,
-      'about':    '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis sollicitudin leo, a luctus nisi. Proin placerat nec velit nec fringilla. Etiam mollis nulla ut nibh venenatis, ac efficitur nisl aliquet. Nunc pharetra rhoncus purus eget ullamcorper. Donec vel massa eu leo pellentesque mattis. Etiam lorem augue, iaculis sit amet metus sed, placerat tempus felis. Ut id elit magna. Cras elementum nunc euismod, ultricies elit vitae, suscipit lectus. Pellentesque ante elit, bibendum porta mauris non, luctus placerat magna.</p>',
-      'link': 'https://www.linkedin.com/company/bruin-spacecraft-group-at-ucla/',
+      'picture':  `${links.people}/karthik.jpg`,
+      'about':    '<p>Second Year, Computer Science and Engineering</p><p>I have a passion for space opera, can read 9 different languages, and I love The Expanse, calligraphy and food.</p>',
+      'link': 'https://www.linkedin.com/in/karthikpullela/',
       'icon': `${links.icons}/linkedin.png`
     },
     {
-      'name':     'Eva Abramson',
+      'name':     'Kyle Johnson',
       'position': 'External Vice President',
-      'picture':  `${links.people}/eva.jpg`,
-      'about':    '',
-      'link': 'https://www.linkedin.com/company/bruin-spacecraft-group-at-ucla/',
+      'picture':  `${links.people}/kyle.jpg`,
+      'about':    '<p>Third Year, Aerospace Engineering</p><p>I enjoy working with space technology, collegiate debate, and cycling.</p>',
+      'link': 'http://www.linkedin.com/in/kylerobertjohnson',
       'icon': `${links.icons}/linkedin.png`
     },
     {
@@ -38,8 +38,8 @@ var context = {
       'name':     'Daniel Hu',
       'position': 'Vice President of Finance',
       'picture':  `${links.people}/daniel.jpg`,
-      'about':    '<p>My only friend</p>',
-      'link': 'https://www.linkedin.com/company/bruin-spacecraft-group-at-ucla/',
+      'about':    '<p>Third Year, Economics</p><p>I am interested in accounting and finance.</p>',
+      'link': 'https://www.linkedin.com/in/danielwilsonhu/',
       'icon': `${links.icons}/linkedin.png`
     }
   ],
