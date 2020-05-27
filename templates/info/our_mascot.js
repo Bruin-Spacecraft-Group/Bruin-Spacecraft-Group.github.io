e9c225c5972a3f52852ee9dbb81a950472f7f776
summerfsiddiqi <62531417+summerfsiddiqi@users.noreply.github.com>
2020-05-26 21:53:57 -0700

    spelling correction

--- templates/info/our_mascot.js
+++ templates/info/our_mascot.js
@@ -3,7 +3,7 @@ var context = {
     "project_description": "The most visible and powerful symbol of our vision is our mascot, Bru. The space bear " +
         "was inspired by UCLA's Bruin Bear and represents the university's heritage, school spirit, and all that " +
         "its student body embodies. Bru's space suit represents the club's focus on the various aspects of " +
-        "research and developement in the realm of space science and technology, including but not limited to " +
+        "research and development in the realm of space science and technology, including but not limited to " +
         "scientific payloads, satelites, and radio communications. Bru in Space is a symbol of Bruin Space's " +
         "unique approach in space mission studies and design, combining well-focused projects within timelines " +
         "structured around the framework of the school year with a light-hearted and fun atmosphere for space " +
