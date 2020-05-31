cd9ab17d30fad5b1f0839f37074a39c57eb3377a
summerfsiddiqi <62531417+summerfsiddiqi@users.noreply.github.com>
2020-05-30 19:19:42 -0700

    Update forge.js

--- templates/projects/forge.js
+++ templates/projects/forge.js
@@ -3,8 +3,10 @@ var context = {
     "project_title": "Educational Project",
     "project_subtitle": "Providing instruction for members in skills applicable to in-house projects.",
     "logo": `${links.logos}/Forgelogo.jpg`,
-    "project_description": "Forge is the education and development program of Bruins Spacecraft Group. We are dedicated to developing the skills of club members to increase the overall skill level of the organization. Intro Series: The Intro Series is a series of workshops intended to give new members the skills they need to be productive members of a project. Workshops: Forge hosts workshops on varying subjects to develop skills that are in high demand within the organization.",
-
+    "project_description": "Forge is the education and development program of Bruins Spacecraft Group. We are dedicated to developing the skills of club members to increase the overall skill level of the organization. \
+    Intro Series is a series of workshops intended to give new members the skills they need to be productive members of a project. \
+    Forge also hosts workshops on varying subjects to develop skills that are in high demand within the organization.",
+    
     "team_description": "",
     "team_photo": `${links.pictures}/forgepeople.JPG`,
     "pm": {
