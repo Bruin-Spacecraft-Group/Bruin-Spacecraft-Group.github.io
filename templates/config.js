89987c7a00a800dce7cc4d7f8f7651e99c62d71b
Andy Peng <andypg20@gmail.com>
2020-10-01 12:47:30 -0700

    config for dev

--- templates/config.js
+++ templates/config.js
@@ -1,13 +1,15 @@
 let localPaths = {
     'andrew': 'C:/Users/andre/OneDrive/Documents/BruinSpace/Bruin-Spacecraft-Group.github.io/',
+    'andy':   '/Users/Andy/Andy/Undergrad/Year_3/websites/Bruin-Spacecraft-Group.github.io',
     'prod':   '',
 };
 
+let localPath = localPaths.andy;
 // let localPath = localPaths.andrew;
-let localPath = localPaths.prod;
+// let localPath = localPaths.prod;
 
-// let html_extension = '.html';
-let html_extension = '';  // uncomment this to make the url prettier
+let html_extension = '.html';
+// let html_extension = '';  // uncomment this to make the url prettier
 
 // Links to images, assets, etc.
 let links = {
