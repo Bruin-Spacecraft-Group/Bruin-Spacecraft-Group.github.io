67e838734b7e73782410489a32e6cc333a674005
ritzz26 <ritagarwalrit@gmail.com>
2023-07-23 15:16:35 +0200

    config setup correction

--- templates/config.js
+++ templates/config.js
@@ -2,11 +2,11 @@ let localPaths = {
     'rit': 'C:/Users/sajal/Documents/College/BruinSpace/Bruin-Spacecraft-Group.github.io',
     'prod':   '',
 };
-let localPath = localPaths.rit;
-//let localPath = localPaths.prod;
+//let localPath = localPaths.rit;
+let localPath = localPaths.prod;
 
-let html_extension = '.html';
-//let html_extension = '';  // uncomment this to make the url prettier
+//let html_extension = '.html';
+let html_extension = '';  // uncomment this to make the url prettier
 
 // Links to images, assets, etc.
 let links = {
