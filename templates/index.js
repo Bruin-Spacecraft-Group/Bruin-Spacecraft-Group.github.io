a3f02023ec1a536c9b21cf5afbe5815478c4c8ed
ritzz26 <ritagarwalrit@gmail.com>
2023-08-03 12:19:30 +0200

    commented out popup interest form and corrected join page interest form link

--- templates/index.js
+++ templates/index.js
@@ -62,3 +62,38 @@ var context = {
 var templateScript = Handlebars.templates.index(context);
 
 $('main').append(templateScript);
+
+// // Get references to the necessary DOM elements
+// const showPopupButton = document.getElementById('showPopup');
+// const popupContainer = document.getElementById('popupContainer');
+
+// // Google Form URL
+// const googleFormUrl = 'https://forms.gle/TM17C2dVXRiEaCQj7';
+
+// // Handle the click event for the "Show Popup" button
+//     // Create the Google Form iframe
+//     const googleFormIframe = document.createElement('iframe');
+//     googleFormIframe.src = googleFormUrl;
+//     googleFormIframe.width = '640';
+//     googleFormIframe.height = '480';
+//     googleFormIframe.frameborder = '0';
+//     googleFormIframe.marginheight = '0';
+//     googleFormIframe.marginwidth = '0';
+//     googleFormIframe.style.border = 'none';
+
+//     // Clear any previous content in the popup container
+//     popupContainer.innerHTML = '';
+
+//     // Append the Google Form iframe to the popup container
+//     popupContainer.appendChild(googleFormIframe);
+
+//     // Show the popup by setting its display style to 'block'
+//     popupContainer.style.display = 'block';
+
+// // Handle the click event outside of the popup to close it
+// document.addEventListener('click', function(event) {
+//     if (event.target.id !== 'showPopup' && !popupContainer.contains(event.target)) {
+//         // Hide the popup by setting its display style to 'none'
+//         popupContainer.style.display = 'none';
+//     }
+// });
