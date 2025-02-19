726050e8f796214bc4e8d24989f32d9aac38ed65
Adrian R. Arana <adrian.r.arana@gmail.com>
2025-02-18 18:08:40 -0800

    Update

--- app/_components/CTABanner.js
+++ app/_components/CTABanner.js
@@ -1,41 +1,14 @@
-"use client";
+'use client'
 
 import React from "react";
-<<<<<<< HEAD
-import Link from "next/link";
-import Image from "next/image";
-import MainButton from "./MainButton";
-=======
 import Link from 'next/link';
 import Image from 'next/image'
 import MainButton from './MainButton'
 import SignUpFormReact from "./Newsletter";
->>>>>>> 15200cd5 (This might break things,)
 // import AutoTyperNL from '../_components/AutoTyperNL';
 // import WaitlistForm from '../_components/WaitlistForm';
 
 export default function CTABanner() {
-<<<<<<< HEAD
-  return (
-    <section className="py-10 md:py-14 h-96 px-10 lg:px-14">
-      <div className="px-10 w-full h-full grid grid-cols-1 md:grid-cols-2 cta-bg rounded-[50px]">
-        <div className="place-self-center">
-          <h1 className="text-4xl text-center md:text-left font-light lg:text-10xl px-wrapper-mobile lg:px-wrapper-desktop lg:mt-0 hidden-br hidden-br--xl">
-            Interested? <br></br>Join BruinSpace Today.
-          </h1>
-        </div>
-        <div className="place-self-center">
-          <div className="flex items-center justify-self-end">
-            <Link href="/join" className="">
-              <MainButton btn_txt="SIGN UP" />
-            </Link>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-}
-=======
     return (
         <section className="py-10 md:py-14 h-96 px-10 lg:px-14">
             <div className='px-10 w-full h-full grid grid-cols-1 md:grid-cols-2 cta-bg rounded-[50px]'>
@@ -56,4 +29,3 @@ export default function CTABanner() {
         </section>
     )
 }
->>>>>>> 15200cd5 (This might break things,)
--- app/_components/MainFooter.js
+++ app/_components/MainFooter.js
@@ -1,140 +1,61 @@
-"use client";
+'use client'
 
 import React from "react";
-import Link from "next/link";
-import Image from "next/image";
-import MainButton3 from "./MainButton3";
-import {
-  SiLinkedin,
-  SiInstagram,
-  SiX,
-  SiDiscord,
-  SiLinktree,
-} from "react-icons/si";
+import Link from 'next/link';
+import Image from 'next/image'
+import MainButton from './MainButton'
+import { SiLinkedin, SiInstagram, SiX } from "react-icons/si";
+
 
 export default function MainFooter() {
-  return (
-    <nav className="container mx-auto min-w-full z-40">
-      <div className="relative bottom-0 left-0 right-0 px-10 lg:px-24 pt-12 pb-2 z-39 bg-bg2 text-bg">
-        <div className="grid grid-cols-4 max-md:grid-cols-1 gap-x-6 max-md:gap-y-8 justify-between justify-items-stretch justify-self-center content-start max-w-max">
-          <div className="justify-self-center content-center">
-            <Link href="/">
-              <Image
-                src="SM - LightSVG.svg"
-                alt="BruinSpace Icon Logo"
-                sizes="100vw"
-                style={{
-                  width: "200px",
-                  height: "200px",
-                }}
-                width={45}
-                height={45}
-              />
-            </Link>
-          </div>
-          <div className="grid items-top justify-between justify-items-stretch gap-2 content-start justify-self-center">
-            <Link
-              className="hover:underline hover:decoration-highlight font-bold"
-              href="/about"
-            >
-              ABOUT
-            </Link>
-            <div className="relative">
-              <Link
-                className="hover:underline hover:decoration-highlight absolute whitespace-nowrap"
-                href="/join"
-              >
-                Join BruinSpace
-              </Link>
-              <br />
+    return (
+        <nav className="container mx-auto min-w-full z-40">
+            <div className="relative bottom-0 left-0 right-0 px-10 lg:px-24 py-12 z-39 bg-bg2 text-bg">
+                <div className='grid grid-cols-6 max-md:grid-cols-1 gap-x-6 max-md:gap-y-8 justify-between justify-items-stretch content-start'>
+                    <div className='max-md:justify-self-center content-center'>
+                        <Link href="/">
+                            <Image
+                                src="SM - LightSVG.svg"
+                                alt="BruinSpace Icon Logo"
+                                sizes="100vw"
+                                style={{
+                                    width: '45px',
+                                    height: '45px',
+                                }}
+                                width={45}
+                                height={45}
+                            />
+                        </Link>
+                    </div>
+                    <div className='max-md:hidden'></div>
+                    <div className='max-md:hidden'></div>
+                    <div className='grid items-top justify-between justify-items-stretch gap-6 content-start justify-self-end max-md:justify-self-center'>
+                        <Link className="hover:underline hover:decoration-highlight" href="/about">
+                            ABOUT
+                        </Link>
+                    </div>
+                    <div className='grid items-top justify-between justify-items-stretch gap-6 content-start justify-self-end max-md:justify-self-center'>
+                        <Link className="hover:underline hover:decoration-highlight" href="/projects">
+                            PROJECTS
+                        </Link>
+                    </div>
+                    <div className='grid items-top justify-between justify-items-stretch gap-6 content-start justify-self-end max-md:justify-self-center'>
+                        <Link className="hover:underline hover:decoration-highlight" href="/partnerships">
+                            PARTNERSHIPS
+                        </Link>
+                    </div>
+                </div>
+                <div className='mt-16 grid grid-cols-2 max-md:grid-cols-1 gap-x-6 max-md:gap-y-8 items-end text-light'>
+                    <div className='justify-self-start max-md:justify-self-center'>
+                        <p className='max-md:text-center'>Bruin Spacecraft Group. <br/>A UCLA student organization.</p>
+                    </div>
+                    <div className='flex flex-row justify-self-end max-md:justify-self-center pb-1'>
+                        <Link href="https://x.com/uclabruinspace" target="_blank" className='ml-6'><SiX /></Link>
+                        <Link href="https://www.linkedin.com/company/bruin-spacecraft-group-at-ucla/" target="_blank" className='ml-6'><SiLinkedin /></Link>
+                        <Link href="https://www.instagram.com/uclabruinspace/" target="_blank" className='ml-6'><SiInstagram /></Link>
+                    </div>
+                </div>
             </div>
-            <Link
-              className="hover:underline hover:decoration-highlight"
-              href="/partnerships"
-            >
-              Partnerships
-            </Link>
-          </div>
-          <div className="grid items-top justify-between justify-items-stretch gap-2 content-start justify-self-center">
-            <Link
-              className="hover:underline hover:decoration-highlight font-bold"
-              href="/projects"
-            >
-              PROJECTS
-            </Link>
-            <Link
-              className="hover:underline hover:decoration-highlight"
-              href="/rapid"
-            >
-              Rapid
-            </Link>
-            <Link
-              className="hover:underline hover:decoration-highlight"
-              href="/overseer"
-            >
-              Overseer
-            </Link>
-            <Link
-              className="hover:underline hover:decoration-highlight"
-              href="/forge"
-            >
-              Forge
-            </Link>
-          </div>
-          <div className="grid items-top justify-between justify-items-stretch gap-6 content-start justify-self-center">
-            <div className="flex flex-col items-center pb-1 text-4xl">
-              <div className="text-bg text-base font-bold">
-                WHERE TO FIND US
-              </div>
-              <Link
-                href="https://linktr.ee/uclabruinspace"
-                target="_blank"
-                className="m-2"
-              >
-                <MainButton3 btn_txt="Linktree" />
-              </Link>
-              <div className="flex flex-row">
-                <Link
-                  href="https://www.instagram.com/bruinspace/"
-                  target="_blank"
-                  className="my-2 mx-3"
-                >
-                  <SiInstagram />
-                </Link>
-                <Link
-                  href="https://www.linkedin.com/company/bruin-spacecraft-group-at-ucla/"
-                  target="_blank"
-                  className="my-2 mx-3"
-                >
-                  <SiLinkedin />
-                </Link>
-              </div>
-              <div className="flex flex-row">
-                <Link
-                  href="https://discord.gg/FWmffUYwaX"
-                  target="_blank"
-                  className="my-2 mx-3"
-                >
-                  <SiDiscord />
-                </Link>
-                <Link
-                  href="https://x.com/uclabruinspace"
-                  target="_blank"
-                  className="my-2 mx-3"
-                >
-                  <SiX />
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="justify-self-center text-center max-md:justify-self-center">
-          <p className="max-md:text-center">
-            Bruin Spacecraft Group. <br />A UCLA student organization.
-          </p>
-        </div>
-      </div>
-    </nav>
-  );
+        </nav>
+    )
 }
